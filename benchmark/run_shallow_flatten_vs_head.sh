2ff0574c78b1addd8c1f5c36f17ceabe64e14854
Patrick Collins <pscollins@google.com>
2026-05-31 16:55:32 +0000

    add new benchmark script

---
+++ benchmark/run_shallow_flatten_vs_head.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Usage:
+#  ./run_mton_vs_mpl.sh [EXTRA_FLAGS]
+
+set -e
+set -x
+
+# Wrapper script to compare baseline MLton vs MLton + shallowFlatte
+
+# Compilers + build flags under test
+MLTON0="../../build/bin/mlton"
+MLTON0_FLAGS=
+MLTON1="../../build/bin/mlton"
+MLTON1_FLAGS='-shallow-flatten-max-iters 1'
+BFLAGS="-mlton '$MLTON0 $MLTON0_FLAGS' -mlton '$MLTON1 $MLTON1_FLAGS'"
+
+EXTRA_FLAGS="$@"
+make test BFLAGS="$BFLAGS $EXTRA_FLAGS"
