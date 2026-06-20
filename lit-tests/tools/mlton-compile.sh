f6e8544f35000950774de8e16e7a57bdac63e9b1
Patrick Collins <pscollins@google.com>
2026-06-20 15:11:53 -0400

    fix bug in OVERRIDE_OUTDIR

--- lit-tests/tools/mlton-compile.sh
+++ lit-tests/tools/mlton-compile.sh
@@ -9,12 +9,27 @@
 set -e
 SCRIPT_DIR=$(dirname $(realpath $0))
 MLTON=${SCRIPT_DIR}/../../build/bin/mlton
-# Out directory must be the last argument
-OUTDIR="${@: -1}"
-# Input file must be the second to last argument, and we make it absolute here
-INFILE=$(realpath "${@: -2: 1}")
-# Everything beforehand gets passed to the compiler
-COMPILE_ARGS=("${@: 1: $# - 2}")
+# Check if OVERRIDE_OUTDIR is provided and non-empty
+if [ -n "$OVERRIDE_OUTDIR" ]; then
+    OUTDIR="$OVERRIDE_OUTDIR"
+    # If the last argument is the input file (ends in .sml, .mlb, etc.),
+    # then there is no output directory argument.
+    LAST_ARG="${@: -1}"
+    if [[ "$LAST_ARG" =~ \.(sml|mlb|fun|sig)(\.disabled)?$ ]]; then
+        INFILE=$(realpath "$LAST_ARG")
+        COMPILE_ARGS=("${@: 1: $# - 1}")
+    else
+        INFILE=$(realpath "${@: -2: 1}")
+        COMPILE_ARGS=("${@: 1: $# - 2}")
+    fi
+else
+    # Out directory must be the last argument
+    OUTDIR="${@: -1}"
+    # Input file must be the second to last argument, and we make it absolute here
+    INFILE=$(realpath "${@: -2: 1}")
+    # Everything beforehand gets passed to the compiler
+    COMPILE_ARGS=("${@: 1: $# - 2}")
+fi
 
 OUTFILE=${OUTDIR}/out.bin
 mkdir -p ${OUTDIR}
