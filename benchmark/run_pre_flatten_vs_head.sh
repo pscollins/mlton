7822e4e1295e1d2893ac277e86a28199d2b99d9a
Patrick Collins <pscollins@google.com>
2026-07-14 14:02:58 +0000

    use wiki fmt

--- benchmark/run_pre_flatten_vs_head.sh
+++ benchmark/run_pre_flatten_vs_head.sh
@@ -14,7 +14,7 @@ MLTON0_FLAGS=
 MLTON1="../../build/bin/mlton"
 # MLTON1_FLAGS='-pre-flatten-max-iters 1 -pre-flatten-consumer-policy all_unpack -pre-flatten-resolve-policy global -pre-flatten-types-policy tuple -pre-flatten-post-steps shrink,flatten,shrink -pre-flatten-recursive-steps 10'
 MLTON1_FLAGS='-pre-flatten-max-iters 1 -pre-flatten-consumer-policy any -pre-flatten-resolve-policy local -pre-flatten-types-policy tuple -pre-flatten-recursive-steps 10'
-BFLAGS="-mlton '$MLTON0 $MLTON0_FLAGS' -mlton '$MLTON1 $MLTON1_FLAGS'"
+BFLAGS="-mlton '$MLTON0 $MLTON0_FLAGS' -mlton '$MLTON1 $MLTON1_FLAGS' -wiki"
 
 EXTRA_FLAGS="$@"
 make test BFLAGS="$BFLAGS $EXTRA_FLAGS"
