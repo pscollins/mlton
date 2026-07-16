d115183b430655ffa988269db4f0fd8185ab6ce2
Patrick Collins <pscollins@google.com>
2026-07-16 19:54:34 +0000

    fix incorrect flag name

--- benchmark/run_pre_flatten_vs_head.sh
+++ benchmark/run_pre_flatten_vs_head.sh
@@ -13,7 +13,7 @@ MLTON0="../../build/bin/mlton"
 MLTON0_FLAGS=
 MLTON1="../../build/bin/mlton"
 # MLTON1_FLAGS='-pre-flatten-max-iters 1 -pre-flatten-consumer-policy all_unpack -pre-flatten-resolve-policy global -pre-flatten-types-policy tuple -pre-flatten-post-steps shrink,flatten,shrink -pre-flatten-recursive-steps 10'
-MLTON1_FLAGS='-pre-flatten-max-iters 1 -pre-flatten-consumer-policy any -pre-flatten-resolve-policy local -pre-flatten-types-policy tuple -pre-flatten-recursive-steps 10 -pre-flatten-phase late -pre-flatten-transfer-policy tail_only'
+MLTON1_FLAGS='-pre-flatten-max-iters 1 -pre-flatten-consumer-policy always -pre-flatten-resolve-policy local -pre-flatten-types-policy tuple -pre-flatten-recursive-steps 10 -pre-flatten-phase late -pre-flatten-transfer-policy tail_only'
 BFLAGS="-mlton '$MLTON0 $MLTON0_FLAGS' -mlton '$MLTON1 $MLTON1_FLAGS'"
 
 EXTRA_FLAGS="$@"
